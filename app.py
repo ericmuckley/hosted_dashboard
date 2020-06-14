d9b30f308eb10e1ca62866d3c0b4b77f05194f1e
Eric Muckley <ericmuckley@gmail.com>
2020-06-14 16:08:59 -0700

    added filtering

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 
 # specify path of data file - should be in same directory as app.py
+# use this to run app on local host
+#DATAPATH = '/Users/emuckley/Documents/GitHub/hosted_dashboard/STARRYDB_interpolated_pp_wc.csv'
+# use this to run app on remote server
 DATAPATH = 'STARRYDB_interpolated_pp_wc.csv'
-
-# CSS stylesheet for page style - should be in same directory as app.py
-STYLESHEET = 'stylesheet.css'
 
 # title for the browser page tab
 TITLE = 'STARRYDB explorer'
@@ -17,27 +17,60 @@ TITLE = 'STARRYDB explorer'
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-def get_dropdown_options(df):
-    """Get the dropdown options to show for plot variables"""
+def format_var(v):
+    """Format a string variable to remove the 'XXX: ' prefix if it exists"""
+    if v is None:
+        return 'None'
+    if isinstance(v, str):
+        return v.split(': ')[1] if ': ' in v else v
+
+
+def get_filtering_options(df):
+    """Get dropdown options to show for filtering variables"""
     options = []
-    for c_raw in list(df):
-        c = c_raw.split(': ')[1] if ': ' in c_raw else c_raw
+    # get list of all column names that aren't ints or floats
+    c_raw_list = list(
+        df.dtypes[df.dtypes != 'int64'][df.dtypes != 'float64'].index)
+    for c_raw in c_raw_list:
+        c = format_var(c_raw)
         options.append({'label': c, 'value': c_raw})
     return options
+
+
+def get_plotting_options(df):
+    """Get the dropdown options to show for plotting variables"""
+    options = []
+    for c_raw in list(df):
+        c = format_var(c_raw)
+        options.append({'label': c, 'value': c_raw})
+    return options
+
 
 def get_labels(col):
     """Get labels to use as hover text over points, based on the
     name of the column thats being plotted"""
     return list(df['FORMULA'].iloc[np.where(df[col].notnull())[0]])
 
+  
+def get_plot_layout(title='Title', xlabel='X-label', ylabel='Y-label'):
+    """Create the layout dictionary that gets passed to the plot"""
+    layout = {
+        'title': title,
+        'xaxis': {'title': xlabel},
+        'yaxis': {'title': ylabel},
+        'margin': {'l': 150, 'r': 150, 'b': 150, 't': 50}}
+    return layout
+     
     
+
 # import data into dataframe
-df = pd.read_csv(DATAPATH)
+df = pd.read_csv(DATAPATH)#[::50]
 # use this for fast testing
 #df = pd.DataFrame(np.random.random((30, 4)), columns=['a','b','c','d'])
 
-options = get_dropdown_options(df)
-
+# get the options to show in the dropdown menus
+plotting_options = get_plotting_options(df)
+filtering_options = get_filtering_options(df)
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -55,10 +88,8 @@ import dash_html_components as html
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # setup the server and app
-app = dash.Dash(__name__, #server=server,
-                external_stylesheets=[STYLESHEET])
+app = dash.Dash(__name__)
 app.title = TITLE
-app.css.config.serve_locally = True
 server = app.server  # this line is required for web hosting
 
 
@@ -76,26 +107,71 @@ app.layout = html.Div(children=[
         html.P([
             html.B('Dataset: '), str(DATAPATH), html.Br(),
             html.B('Total rows: '), str(df.shape[0]), html.Br(),
-            html.B('Total columns: '), str(df.shape[1]), html.Br()])]),
+            html.B('Total columns: '), str(df.shape[1]), html.Br(),
+            ])]),
+
+    html.Br(),
+
+
+    html.Div([html.P([
+        html.B('Select a variable to plot on the X-axis.')
+        ])]),
+
 
     # division for the X dropdown menu
     html.Div([
         dcc.Dropdown(
             id='x_var_dropdown',
-            options=options,
+            clearable=False,
+            options=plotting_options,
+            value=plotting_options[1]['value'],
             placeholder="Select variable for X-axis (optional)",
             style=dict(width='60%', verticalAlign="middle"))]),
+    
+    
+    
+    html.Div([html.P([
+        html.B('Select a variable to plot on the Y-axis.'),
+        ])]),
+    
+    
 
     # division for the Y dropdown menu
     html.Div([
         dcc.Dropdown(
             id='y_var_dropdown',
-            options=options,
+            clearable=False,
+            options=plotting_options,
+            value=plotting_options[2]['value'],
             placeholder="Select variable for Y-axis",
             style=dict(width='60%', verticalAlign="middle"))]),
     
+ 
+    
+    html.Div([html.P([
+        html.B('Select a variable which filters the results.'),
+               ])]),
+    
+    # division for the filtering dropdown menu
+    html.Div([
+        dcc.Dropdown(
+            id='filter_var_dropdown',
+            options=filtering_options,
+            placeholder="Select variable for filtering (optional)",
+            style=dict(width='60%', verticalAlign="middle"))]),   
+    
+    
+    html.Div([html.P([
+            'To hide a filter category from the plot, click its entry in the plot legend.',
+            html.Br(),
+            'To show only one filter category in the plot, double-click that category in the plot legend.'
+            ])]),
+
+    html.Br(),
+
     # create plot
     dcc.Graph(id='graph', config={"displaylogo": False})
+
 
 ])
 
@@ -106,53 +182,77 @@ app.layout = html.Div(children=[
 @app.callback(
     dash.dependencies.Output('graph', 'figure'),
     [dash.dependencies.Input('x_var_dropdown', 'value'),
-     dash.dependencies.Input('y_var_dropdown', 'value')])
-def update_graph(X, Y):
+     dash.dependencies.Input('y_var_dropdown', 'value'),
+     dash.dependencies.Input('filter_var_dropdown', 'value')])
+def update_graph(X, Y, F):
     """Update the graph when variable selections are changed"""
     
-    # take care of all edge cases in dropdown menus
-    if X is None and Y is None:
-        x, y = None, None
-        record_num = 0
-        hover_text = None
+    dfs = df[[X, Y]].dropna()
+    x = list(dfs[X])
+    y = list(dfs[Y])
+    record_num = len(y)  
+    hover_text = get_labels(Y)
 
-    elif X is None and Y is not None:
-        y = list(df[Y].dropna())
-        x = list(np.arange(len(y))+1)
-        record_num = len(y)
-        hover_text = get_labels(Y)
+
+    # if we're not filtering the data, plot all of it
+    if F is None:
+        data_list = [
+                {'x': x, 'y': y,
+                 'text': hover_text,
+                 'hoverinfo': 'text',
+                 'mode': 'markers',
+                 'marker': {'size': 6}}
+                ]
+        filter_categories = []
+
+
+
+    # if we're filtering the data, split it into different groups
+    else:
+        # get filter categories
+        filter_categories = list(np.unique(df[F].astype(str)))
         
-    elif Y is None and X is not None:
-        x = list(df[X].dropna())
-        y = list(np.zeros_like(x))
-        record_num= len(y)
-        hover_text = get_labels(X)
     
-    elif X is not None and Y is not None:
-        dfs = df[[X, Y]].dropna()
-        x = list(dfs[X])
-        y = list(dfs[Y])
-        record_num= len(y)  
-        hover_text = get_labels(Y)
+        # create list of data dictionaries to be plotted. each dict has form:
+        #{'x': x, 'y': y, 'mode': 'markers', 'marker': {'size': 6}, **kwargs}
+        data_list = []
+        for i in range(len(filter_categories)):
+            data_list.append({
+                'x': [], 'y': [], 'text': [],
+                'name': filter_categories[i],
+                'hoverinfo': 'text',
+                'mode': 'markers',
+                'marker': {'size': 6}})
 
-    # error catching for axis labels
-    X = 'None' if X is None else X
-    Y = 'None' if Y is None else Y
 
-    return {
-        'data': [
-            {'x': x,
-             'y': y,
-             'text': hover_text,
-             'hoverinfo': 'text',
-             'mode': 'markers',
-             'marker': {'size': 6}}
-            ],
-        'layout': {'title':'Plotting {} records'.format(record_num),
-                   'xaxis': {'title': X},
-                   'yaxis': {'title': Y},
-                   'margin': {'l': 150, 'r': 150, 'b': 150, 't': 50}}
-        }
+        # now populate the data list with data
+        # loop over each record and add it to the data
+        for i in range(len(df)):
+            
+            # loop over each dtaset in the data list
+            for ds in data_list:
+                if df[F].iloc[i] == ds['name']:
+                    ds['x'].append(df[X].iloc[i])
+                    ds['y'].append(df[Y].iloc[i])
+                    ds['text'].append(df['FORMULA'].iloc[i])
+
+
+    # create plot layout
+    layout = get_plot_layout(
+            title='Showing {} records filtered by {} categories'.format(
+                record_num, len(filter_categories)),
+            xlabel=format_var(X),
+            ylabel=format_var(Y))
+
+
+    return {'data': data_list, 'layout': layout}
+            
+
+
+
+
+
+
 
 
 '''
